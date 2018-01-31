46dcf90ed151a503c4498002434b84c685b70292
John-David Dalton <john.david.dalton@gmail.com>
2018-01-30 23:21:12 -0800

    backport resolve CVE-2018-3721 based on lodash/lodash commit d8e069cc3

--- test/test.js
+++ test/test.js
@@ -7554,6 +7554,21 @@
         skipAssert(assert);
       }
     });
+
+    QUnit.test('should not merge "__proto__" properties', function(assert) {
+      assert.expect(1);
+
+      if (JSON) {
+        _.merge({}, JSON.parse('{"__proto__":{"a":1}}'));
+
+        var actual = "a" in objectProto;
+        delete objectProto.a;
+
+        assert.notOk(actual);
+      } else {
+        skipAssert(assert);
+      }
+    });
   }());
 
   /*--------------------------------------------------------------------------*/
